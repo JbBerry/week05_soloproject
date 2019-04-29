aa463ac11ce279163964727535ddf115dd9ced82
JamesBerry <a9883805@9883805s-MacBook-Pro.local>
2019-04-29 11:13:15 +0100

    client folder structure added

---
+++ client/src/app.js
@@ -0,0 +1,22 @@
+const AssessmentModel = require('./models/assessment_model.js');
+const MarkModel = require('./models/mark_model.js');
+const StudentModel = require('./models/student_model.js');
+
+const AssessmentFormView = require('./views/assessment_form_view.js');
+const MarkFormView = require('./views/mark_form_view.js');
+const StudentFormView = require('./views/student_form_view.js');
+const StudentView = require('./views/student_view.js');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // const characterContainer = document.querySelector('#character-container');
+  // const characterView = new CharacterView(characterContainer);
+  // characterView.bindEvents();
+  //
+  // const characters = new Characters();
+  // characters.getData();
+  //
+  // const formElement = document.querySelector('#character-form');
+  // const characterFormView = new CharacterFormView(formElement);
+  // characterFormView.bindEvents();
+});
